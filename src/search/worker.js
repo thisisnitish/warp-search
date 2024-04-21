0a585117867743667a6db1bae464edffbeb4ff34
Nitish Kumar <thisisnitish30@gmail.com>
2024-04-21 12:27:29 +0530

    added nested search functionality

--- src/search/worker.js
+++ src/search/worker.js
@@ -16,9 +16,14 @@ export default () => {
     if (value == "") return data;
 
     const filteredData = data.filter((record) => {
-      return Object.keys(record).some((key) =>
-        String(record[key]).toLowerCase().includes(value.toLowerCase())
-      );
+      return Object.keys(record).some((key) => {
+        // Perform the nested search i.e. Recursive search!!
+        if (Array.isArray(record[key])) {
+          record[key] = performSearch(record[key], value);
+          return record[key].length;
+        }
+        return String(record[key]).toLowerCase().includes(value.toLowerCase());
+      });
     });
 
     // console.log('filteredWorker: ', filteredData, data, value)
